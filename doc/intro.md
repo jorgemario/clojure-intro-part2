ba70d4bb523ae09523df58c7a93fc9a18953d4e3
Jorge Gomez <jorgemario@belucasys.com>
2016-08-15 16:09:07 -0500

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojure-intro-part2
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojure_intro_part2/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-intro-part2.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-intro-part2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
